de15390bd18b9a20269421e798fe8f6878a01a5c
Karl Benedict <kbene@unm.edu>
2023-11-27 08:48:23 -0700

    add principles, libraries, process script

--- app.R
+++ app.R
@@ -6,6 +6,9 @@
 
 # Libraries
 library(shiny)
+library(jsonlite)
+library(tidyverse)
+library(tidyr)
 
 # Setup 
 principles_labels <- c(
@@ -21,14 +24,30 @@ principles_names <- c(
 names(principles_names) <- principles_labels
 principles_files <- c(
   "source-data/fair.json",
-  "source-data/geo-ds",
-  "source-data/geo-dm"
+  "source-data/geo-ds.json",
+  "source-data/geo-dm.json"
 )
 names(principles_files) <- principles_names
 
 
 # Functions
-
+get_principles <- function(source_files) {
+  dt <- tibble(
+    name = character(),
+    label = character(),
+    description = character()
+  )
+  for (file_path in source_files) {
+    #print(file_path)
+    principles <- tibble(package = fromJSON(file_path))
+    
+    
+    
+    dt <- bind_rows(dt, unnest_wider(principles, items))
+  }
+  return(dt)
+}
+out <- get_principles(principles_files)
 
 # Application Code
 ui <- fluidPage(
